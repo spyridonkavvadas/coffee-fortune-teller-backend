1f9c51a7b44924d177260a70686ee8b40ce6877d
Spyridon Kavvadas <kavvadasspyridon@gmail.com>
2021-02-08 19:45:56 +0100

    Complete random oracle returns

--- controllers/oracles.js
+++ controllers/oracles.js
@@ -17,15 +17,29 @@ const getOracles = async (req, res, next) => {
 
 const getSingleOracle = async (req, res, next) => {
   try {
-    
     const healthOracle = await Oracle.aggregate([{ $match: { category: 'health' } }]).sample(1);
 
-const loveOracle = await Oracle.aggregate([{ $match: { category: 'love' } }]).sample(1);
+    const travelsOracle = await Oracle.aggregate([{ $match: { category: 'travels' } }]).sample(1);
 
-const result = healthOracle[0].predictions + ' ' + loveOracle[0].predictions;
+    const businessOracle = await Oracle.aggregate([{ $match: { category: 'business' } }]).sample(1);
+
+    const financialOracle = await Oracle.aggregate([{ $match: { category: 'financial' } }]).sample(1);
+
+    const loveOracle = await Oracle.aggregate([{ $match: { category: 'love' } }]).sample(1);
+
+    const familyOracle = await Oracle.aggregate([{ $match: { category: 'family' } }]).sample(1);
+
+    const relationshipsOracle = await Oracle.aggregate([{ $match: { category: 'relationships' } }]).sample(1);
+
+    const successOracle = await Oracle.aggregate([{ $match: { category: 'success' } }]).sample(1);
+
+
+    const result = healthOracle[0].predictions + ' ' + travelsOracle[0].predictions + ' ' + businessOracle[0].predictions + ' ' + financialOracle[0].predictions + ' ' + loveOracle[0].predictions + ' ' + familyOracle[0].predictions + ' ' + relationshipsOracle[0].predictions + ' ' + successOracle[0].predictions;
+
+
     console.log(healthOracle[0].predictions);
     console.log(result);
-    
+      
     res.json({ success: true, data: result })
   } catch(err) {
     next(err)
